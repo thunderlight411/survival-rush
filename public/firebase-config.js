91351bedb3c3243e77e8c9e929de8a99711067a2
Marco <4320849+thunderlight411@users.noreply.github.com>
2026-04-18 15:01:20 +0200

    Activate Firebase serverless multiplayer with project config

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -8,4 +8,13 @@
 //   databaseURL: "https://your-project-id-default-rtdb.europe-west1.firebasedatabase.app"
 // };
 
-window.FIREBASE_CONFIG = null;
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBOrqrFIyAlOOHU8M_gjFaF4MCDe_fazJI",
+  authDomain: "survival-rush-9b6da.firebaseapp.com",
+  databaseURL: "https://survival-rush-9b6da-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "survival-rush-9b6da",
+  storageBucket: "survival-rush-9b6da.firebasestorage.app",
+  messagingSenderId: "810294201064",
+  appId: "1:810294201064:web:83dc21c073987ec3ec975d",
+  measurementId: "G-EKP01S7Y54"
+};
